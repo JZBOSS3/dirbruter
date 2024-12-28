1c9403592db557da278a0b2dda1d8a12918b27dc
jzboss3 <66354470+JZBOSS3@users.noreply.github.com>
2024-12-28 17:10:41 +0200

    Update dirbruter.py

--- dirbruter.py
+++ dirbruter.py
@@ -1,133 +1,254 @@
-import requests
 import sys
 import socket
+import requests
 from colorama import Fore, Style
-from datetime import datetime
 
-verbose = False
-quiet = False
-write = False
-write_file_name = ''
-directories_file = ''
-base_url = ''
-wrote_target = False
+domain = ""
+domains_file = ""
+directories_file = "big.txt"
 
-def print_banner():
-    print(Fore.YELLOW + '-'*50 + Style.RESET_ALL)
-    print(Fore.RED + "Usage: script.py [options] <url> -f <directories_file>" + Style.RESET_ALL)
-    print(Fore.MAGENTA + 'Options:' + Style.RESET_ALL)
-    print(Fore.WHITE + '-h\tDisplay Help Menu' + Style.RESET_ALL)
-    print(Fore.WHITE + '-f\tSpecify File Containing List of Directories' + Style.RESET_ALL)
-    print(Fore.WHITE + '-v\tVerbose Mode' + Style.RESET_ALL)
-    print(Fore.WHITE + '-q\tQuiet Mode' + Style.RESET_ALL)
-    print(Fore.WHITE + '-o\tOutput To File' + Style.RESET_ALL)
-    print(Fore.YELLOW + '-'*50 + Style.RESET_ALL)
+s200 = []  # 200 OK
+s301 = []  # 301 Moved Permanently
+s403 = []  # 403 Forbidden
+s404 = []  # 404 Not Found
+s405 = []  # 405 Method Not Allowed
+s500 = []  # 500 Internal Server Error
 
-def file_exists(arguments):
-    exists = False
-    for argument in arguments:
-        if argument == '-f':
-            exists = True
-    return exists
+html_ascii_title = r'''
+────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+─────────██████─██████████████████─██████████████───██████████████─██████████████─██████████████─██████████████─
+─────────██░░██─██░░░░░░░░░░░░░░██─██░░░░░░░░░░██───██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+─────────██░░██─████████████░░░░██─██░░██████░░██───██░░██████░░██─██░░██████████─██░░██████████─██████████░░██─
+─────────██░░██─────────████░░████─██░░██──██░░██───██░░██──██░░██─██░░██─────────██░░██─────────────────██░░██─
+─────────██░░██───────████░░████───██░░██████░░████─██░░██──██░░██─██░░██████████─██░░██████████─██████████░░██─
+─────────██░░██─────████░░████─────██░░░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+─██████──██░░██───████░░████───────██░░████████░░██─██░░██──██░░██─██████████░░██─██████████░░██─██████████░░██─
+─██░░██──██░░██─████░░████─────────██░░██────██░░██─██░░██──██░░██─────────██░░██─────────██░░██─────────██░░██─
+─██░░██████░░██─██░░░░████████████─██░░████████░░██─██░░██████░░██─██████████░░██─██████████░░██─██████████░░██─
+─██░░░░░░░░░░██─██░░░░░░░░░░░░░░██─██░░░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+─██████████████─██████████████████─████████████████─██████████████─██████████████─██████████████─██████████████─
+────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+'''
 
-def checkArguments(arguments):
-    if len(arguments) < 3:
-        print_banner()
+# Function to generate the ASCII logo
+def print_ascii():
+    ascii_txt = r'''
+██████████████████████████████████████████████████████
+█▄─▄▄▀█▄─▄█▄─▄▄▀█▄─▄─▀█▄─▄▄▀█▄─██─▄█─▄─▄─█▄─▄▄─█▄─▄▄▀█
+██─██─██─███─▄─▄██─▄─▀██─▄─▄██─██─████─████─▄█▀██─▄─▄█
+▀▄▄▄▄▀▀▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀'''
+    print(Fore.GREEN + ascii_txt + Style.RESET_ALL)
+
+# Function to print the help menu
+def print_help_menu():
+    print(Fore.BLUE + 40*'*' + Style.RESET_ALL)
+    print(Fore.RED + 'Usage python3 dirbruter.py -d example.com' + Style.RESET_ALL)
+    print(Fore.BLUE + 40*'*' + Style.RESET_ALL)
+    print(Fore.YELLOW + "Flags:" + Style.RESET_ALL)
+    print("-d\tDomain to brute force")
+    print("-df\tFile containing Domains to brute force")
+    print("-f\tFile Containing directories")
+    print("-h\tPrint help menu")
+    print('')
+    sys.exit()
+
+# Check if the domain is alive
+def web_alive(domain):
+    return requests.get(f'https://{domain}').status_code == 200 or requests.get(f'http://{domain}').status_code == 200
+
+# Scan the domain for directories
+def scan_domain(domain, directory_file):
+    global s200, s301, s403, s404, s405, s500
+    if not web_alive(domain):
+        print(f"{Fore.RED}[!] {domain} is not alive{Style.RESET_ALL}")
+    else:
+        try:
+            with open(directory_file, mode='r') as directories:
+                for directory in directories:
+                    url = f'https://{domain}/{directory.strip()}'
+                    response = requests.get(url)
+                    sc = response.status_code
+                    if sc == 200:
+                        s200.append(url)
+                        print(Fore.GREEN + f'[!]{sc} Found: {url}' + Style.RESET_ALL)
+                    elif sc == 301:
+                        s301.append(url)
+                    elif sc == 403:
+                        s403.append(url)
+                        print(Fore.RED + f'[!]{sc} Forbidden: {url}' + Style.RESET_ALL)
+                    elif sc == 404:
+                        s404.append(url)
+                        print(Fore.RED + f'[!]{sc} Not Found: {url}' + Style.RESET_ALL)
+                    elif sc == 405:
+                        s405.append(url)
+                    elif sc == 500:
+                        s500.append(url)
+        except FileNotFoundError:
+            print(f"{Fore.RED}[!] {directory_file} not found{Style.RESET_ALL}")
+
+# Function to generate the HTML page
+def generate_html_page():
+    # HTML report template
+    html_content = f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Security Scan Report</title>
+    <style>
+        body {{
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f9;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }}
+        .container {{
+            width: 80%;
+            margin: 30px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }}
+        h1 {{
+            text-align: center;
+            color: #4CAF50;
+        }}
+        .scan-results {{
+            font-size: 1em;
+            line-height: 1.6;
+        }}
+        .status-group {{
+            margin-bottom: 20px;
+            padding-left: 20px;
+        }}
+        .status-group h3 {{
+            color: #333;
+        }}
+        .status-group ul {{
+            list-style-type: none;
+            padding: 0;
+        }}
+        .status-group li {{
+            padding: 5px;
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <p style="font-size: .5rem; width: 50%; margin: 0 auto;" align="center">{html_ascii_title}</p>
+        <div class="scan-results">
+            <div class="status-group" style="border-left: 5px solid green;">
+                <h3>200 OK</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s200])}
+                </ul>
+            </div>
+            <div class="status-group" style="border-left: 5px solid blue;">
+                <h3>301 Moved Permanently</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s301])}
+                </ul>
+            </div>
+            <div class="status-group" style="border-left: 5px solid orange;">
+                <h3>403 Forbidden</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s403])}
+                </ul>
+            </div>
+            <div class="status-group" style="border-left: 5px solid red;">
+                <h3>404 Not Found</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s404])}
+                </ul>
+            </div>
+            <div class="status-group" style="border-left: 5px solid yellow;">
+                <h3>405 Method Not Allowed</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s405])}
+                </ul>
+            </div>
+            <div class="status-group" style="border-left: 5px solid purple;">
+                <h3>500 Internal Server Error</h3>
+                <ul>
+                    {"".join([f"<li>{url}</li>" for url in s500])}
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+"""
+
+    # Write HTML content to a file with utf-8 encoding
+    with open("scan_report.html", "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+    print(Fore.GREEN + "HTML Report has been generated: scan_report.html" + Style.RESET_ALL)
+
+
+def ask_to_generate():
+    answer = input('Do You Want To Generate an HTML Page Report(yes/no)? ')
+    if(answer == 'yes'):
+        generate_html_page()
+    else:
+        print(Fore.BLUE + "Thank you for using my tool!" + Style.RESET_ALL)
         sys.exit()
-    if not file_exists(arguments):
-        print_banner()
-        sys.exit()
-    i = 0
-    global verbose, quiet, write, directories_file, base_url, write_file_name
-    for arg in arguments:
-        if arg == '-h':
-            print_banner()
-            sys.exit()
-        elif arg == '-v':
-            verbose = True
-        elif arg == '-q':
-            quiet = True
-        elif arg == '-o':
-            write = True
-            write_file_name = arguments[i + 1]
-        elif arg == '-f':
-            directories_file = arguments[i + 1]
-        elif arg.startswith('https:') or arg.startswith('http:'):
-            base_url = arguments[i]
-        i += 1
 
-def print_ip_target(target):
-    target = target.split('www.')
-    return socket.gethostbyname(target[1])
-    
+# Main function to run the tool
+def run_tool():
+    global domain, domains_file, directories_file
+    flags = ['-h', '-f', '-d', '-df']
+    args = sys.argv[1:]
 
-def print_top():
-    print(Fore.YELLOW + '-'*50 + Style.RESET_ALL)
-    print(Fore.BLUE + f'\tStarted Scanning Target: {base_url}/' + Style.RESET_ALL)
-    print(Fore.LIGHTBLACK_EX + f'\tTarget IP Address: {print_ip_target(base_url)}' + Style.RESET_ALL)
-    file = directories_file.split('\\')
-    print(Fore.MAGENTA + f'\tUsed File: {file[len(file) - 1]}' + Style.RESET_ALL)
-    print(Fore.GREEN + "\tTime Started " + str(datetime.now()) + Style.RESET_ALL)
-    print(Fore.YELLOW + '-'*50 + Style.RESET_ALL)
+    if len(args) < 2:
+        print_help_menu()
+    else:
+        skip_next = False
+        for i, arg in enumerate(args):
+            if skip_next:
+                skip_next = False
+                continue
 
-   
-def write_file(url, msg):
-    try:
-        with open(write_file_name, mode='a') as file:
-            global wrote_target
-            if not wrote_target:
-                file.write('-'*50 + '\n')
-                file.write(f'\tStarted Scanning Target: {base_url}/' + '\n')
-                file.write(f'\tTarget IP Address: {print_ip_target(base_url)}' + '\n')
-                dir_file = directories_file.split('\\')
-                file.write(f'\tUsed File: {dir_file[len(dir_file) - 1]}' + '\n')
-                file.write("\tTime Started " + str(datetime.now()) + '\n')
-                file.write('-'*50 + '\n')
-                wrote_target = True
-
-            file.write(f'{url}{msg}\n')
-    except IOError:
-        print('Error Writing To The File... Try Again!')
-    except Exception as e:
-        print(f'Exception: {e}')
-
-arguments = sys.argv
-checkArguments(arguments)
-print_top()
-
-try:
-    with open(directories_file, mode='r') as file:
-        for line in file:
-            directory = line.strip()
-            url = f"{base_url}/{directory}"
-            response = requests.get(url)
-            if not quiet:
-                if response.status_code == 200:
-                    print(Fore.GREEN + f'{url}: Code 200' + Style.RESET_ALL)
-                    if write:
-                        write_file(url, ': Code 200')
-                if verbose:    
-                    if response.status_code == 301:
-                        print(Fore.LIGHTBLACK_EX + f'{url}: Code 301' + Style.RESET_ALL)
-                        if write:
-                            write_file(url, ': Code 301')
-                    elif response.status_code == 404:
-                        print(Fore.RED + f'{url}: Code 404' + Style.RESET_ALL)
-                        if write:
-                            write_file(url, ': Code 404')
-                    else:
-                        print(Fore.LIGHTYELLOW_EX + f'{url}: Code {response.status_code}' + Style.RESET_ALL)
-                        if write:
-                            write_file(url, ': Code ' + str(response.status_code))
+            if arg not in flags:
+                print_help_menu()
             else:
-                if write:
-                    write_file(url, ': Code ' + str(response.status_code))
+                if arg == '-d':
+                    domain = args[i + 1]
+                    skip_next = True
+                elif arg == '-df':
+                    domains_file = args[i + 1]
+                    skip_next = True
+                elif arg == '-f':
+                    directories_file = args[i + 1]
+                    skip_next = True
 
-except FileNotFoundError:
-    print(f"Error: File '{directories_file}' not found.")
-except requests.RequestException as e:
-    print(f"Error: An error occurred with the request: {e}")
-except KeyboardInterrupt:
-    print('Exiting Program...')
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+        print(Fore.GREEN + 'Starting Scan...' + Style.RESET_ALL)
+
+        try:
+            if domain:
+                print(Fore.CYAN + f'Scanning {domain}\tIP: {socket.gethostbyname(domain.replace("www.", ""))}' + Style.RESET_ALL)
+        except socket.gaierror:
+            print(Fore.RED + "Domain name could not be resolved." + Style.RESET_ALL)
+            sys.exit()
+
+        if domain and not domains_file:
+            scan_domain(domain, directories_file)
+        elif not domain and domains_file:
+            with open(domains_file, mode='r') as file:
+                for domain in file:
+                    scan_domain(domain.strip(), directories_file)
+        else:
+            print_help_menu()
+
+        ask_to_generate()
+
+if __name__ == "__main__":
+    try:
+        print_ascii()
+        run_tool()
+    except KeyboardInterrupt:
+        ask_to_generate()
+        print(Fore.RED + "\nExiting..." + Style.RESET_ALL)
